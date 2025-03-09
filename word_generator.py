bf0fcfa551d690633b26ef2f1da423837cb4613d
Saakshar <81653007+Saakshar@users.noreply.github.com>
2025-03-08 23:24:45 -0500

    Update word_generator.py

--- word_generator.py
+++ word_generator.py
@@ -3,13 +3,16 @@ from selenium.webdriver.common.by import By
 options=webdriver.ChromeOptions()
 options.add_experimental_option("detach",True)
 d=webdriver.Chrome(options=options)
-d.get("https://www.thefreedictionary.com/6-letter-words.htm")
-words=d.find_elements(By.CSS_SELECTOR, "li a")
+d.get("https://www.wordsdetail.com/6-letter-words/")
+words=d.find_elements(By.CSS_SELECTOR, "ul li")
 with open("words.csv","w") as file:
     file.write("")
 with open("words.csv", "a") as file:
     for word in words:
-        if len(word.text)==6:
-            file.write(f"{word.text.lower()}\n")
+        try:
+            if len(word.text)==6:
+                file.write(f"{word.text.lower()}\n")
+        except:
+            pass
     file.write("wordle")
 d.quit()
